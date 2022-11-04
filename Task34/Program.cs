575d94d99d653ba4e69bda6b43d01200fcb53c14
leonid ivanov <leonid.ivanov.w@gmail.com>
2022-11-04 16:53:47 +0500

    Updated 34

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -1,19 +1,16 @@
 ﻿int[] array = new int[6];
-int count = 0;
 FillArray(array);
 void FillArray(int[] a)
 {
+    int count = 0;
     for (int i = 0; i < a.Length; i++)
     {
         a[i] = new Random().Next(100, 1000);
+        System.Console.WriteLine($"The {i} index is: {a[i]}");
+        if (a[i] % 2 == 0)
+        {
+            count++;
+        }
     }
+    System.Console.WriteLine($"The total amount of even numbers is: {count}");
 }
-for (int i = 0; i < array.Length; i++)
-{
-    System.Console.WriteLine($"The {i} index is: {array[i]}");
-    if (array[i] % 2 == 0)
-    {
-        count++;
-    }
-}
-System.Console.WriteLine($"The total amount of even numbers is: {count}");
